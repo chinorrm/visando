8af6088071c0435de4d0653c626315c83182be49
rrmora <raul.remo02@gmail.com>
2019-04-05 21:11:18 -0600

    Se agregaron los componentes para la navegacion

--- VisandoApp/src/app/app.module.ts
+++ VisandoApp/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './componentes/home/home.component';
+import { NavbarComponent } from './componentes/share/navbar/navbar.component';
+import { RegisterComponent } from './componentes/register/register.component';
+import { LoginComponent } from './componentes/login/login.component';
+import { MainLayoutComponent } from './componentes/main-layout/main-layout.component';
+import { PageNotFoundComponent } from './componentes/page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavbarComponent,
+    RegisterComponent,
+    LoginComponent,
+    MainLayoutComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
