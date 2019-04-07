4d8ac65674b96bd7fbd9624490f0705cae034a33
rrmora <raul.remo02@gmail.com>
2019-04-06 23:24:21 -0600

    Se agrego firebase al proyecto

--- VisandoApp/src/environments/environment.prod.ts
+++ VisandoApp/src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyDIFkXyFDcf7URqS2mj6zUrGQl5Z25Ot8E',
+    authDomain: 'visas-data-base.firebaseapp.com',
+    databaseURL: 'https://visas-data-base.firebaseio.com',
+    projectId: 'visas-data-base',
+    storageBucket: 'visas-data-base.appspot.com',
+    messagingSenderId: '163074820498'
+  }
 };
--- VisandoApp/src/environments/environment.ts
+++ VisandoApp/src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyDIFkXyFDcf7URqS2mj6zUrGQl5Z25Ot8E',
+    authDomain: 'visas-data-base.firebaseapp.com',
+    databaseURL: 'https://visas-data-base.firebaseio.com',
+    projectId: 'visas-data-base',
+    storageBucket: 'visas-data-base.appspot.com',
+    messagingSenderId: '163074820498'
+  }
 };
 
 /*
