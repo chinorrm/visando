dce53adf9c81e836eb84e511ec55042d14576c9f
rrmora <raul.remo02@gmail.com>
2019-04-22 19:50:24 -0500

    login y logout

--- VisandoApp/src/app/componentes/login/login.component.ts
+++ VisandoApp/src/app/componentes/login/login.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
+
+
 
 @Component({
   selector: 'app-login',
@@ -7,9 +11,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  public email: string;
+  public password: string;
+  constructor(private authService: AuthService, private router: Router) { }
 
   ngOnInit() {
   }
+
+  login() {
+    this.authService.login(this.email, this.password)
+    .then((result) => {
+      this.router.navigate(['/main']);
+    }).catch((err) => {
+      console.log(err);
+      this.router.navigate(['/login']);
+    });
+  }
+
+
+
 
 }
--- VisandoApp/src/app/componentes/share/navbar/navbar.component.ts
+++ VisandoApp/src/app/componentes/share/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../../services/auth.service';
 
 @Component({
   selector: 'app-navbar',
@@ -6,10 +7,26 @@ import { Component, OnInit } from '@angular/core';
   styles: []
 })
 export class NavbarComponent implements OnInit {
-
-  constructor() { }
+  public isLoged: boolean;
+  public usuario: string;
+  public emailUser: string;
+  constructor(private authService: AuthService) { }
 
   ngOnInit() {
+    this.authService.getUserAuth().subscribe(auth => {
+      if (auth) {
+        this.isLoged = true;
+        this.usuario = auth.displayName;
+        this.emailUser = auth.email;
+      } else {
+        this.isLoged = false;
+      }
+
+    });
+  }
+
+  logout() {
+    this.authService.logout();
   }
 
 }
